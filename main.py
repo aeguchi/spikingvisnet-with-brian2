b19aa6677dfd2d003c3b697c591ef2d4a1286a13
aeguchi <aeguchi@localhost>
2015-06-26 17:47:19 +0100

    added : load all the training images in a specified folder and apply gabor filter

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from Parameters import *
 from imageImport import *
 import sys
+import pylab, glob
 
 #construct NetworkState
 
 #Training
-#load gabor filtered ImageSurface
+trainingImages = glob.iglob("./images/training/*.png")
+for img_fn in trainingImages:
+    #load gabor filtered ImageSurface
+    img = cv2.imread(img_fn)
+    if img is None:
+        print 'Failed to load image file:', img_fn
+        sys.exit(1)
+    filters = build_filters()
+    res = process(img, filters)
     
-img_fn = 'test2.png'
-img = cv2.imread(img_fn)
-if img is None:
-    print 'Failed to load image file:', img_fn
-    sys.exit(1)
-filters = build_filters()
-res = process(img, filters)
-
-for r in res:
-    cv2.imshow('result', r)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    for r in res:
+        cv2.imshow('result', r)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
     
+    #To-DO: trainNetworkWith
+    
+#Testing
