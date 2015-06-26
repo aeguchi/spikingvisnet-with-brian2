f8eae647b8292d70b30de4e1c8c32a5ffbc0d087
aeguchi <aeguchi@localhost>
2015-06-26 17:31:33 +0100

    gaborFilter working code

---
+++ Parameters.py
@@ -0,0 +1,19 @@
+import numpy as np
+from cmath import sqrt, log
+
+#class Parameters:
+nStim = 2
+nTrans = 4
+
+ksize = 31  # 31 the size of the Gabor kernel. If ksize = (a, b), we then have a Gabor kernel of size a x b pixels. As with many other convolution kernels, ksize is preferably odd and the kernel is a square (just for the sake of uniformity).
+bw = 1.5 #spatial bandwidth:
+thetaList = [0, np.pi/4, np.pi/2, np.pi*3/4]  #0 #orientation: the orientation of the normal to the parallel stripes of the Gabor function.
+lamdaList = [2.0, 16.0]; #wavelength: the wavelength of the sinusoidal factor in the above equation.
+gamma = 0.5 #aspect ratio:  the spatial aspect ratio.
+#psiList = [0, np.pi, -np.pi/2, np.pi/2]#phase shift:  phase offset
+psiList = [0]#phase shift:  phase offset
+imageFolder = "images"
+sigma = 0.5 # the standard deviation of the Gaussian function used in the Gabor filter.
+	
+	#def __init( self):
+		
--- imageImport.py
+++ imageImport.py
@@ -2,48 +2,41 @@
  
 import numpy as np
 import cv2
+from Parameters import *
+import matplotlib.pyplot as plt
+import math
+from numpy import mean
  
 def build_filters():
     filters = []
-    ksize = 3#31 the size of the Gabor kernel. If ksize = (a, b), we then have a Gabor kernel of size a x b pixels. As with many other convolution kernels, ksize is preferably odd and the kernel is a square (just for the sake of uniformity).
-#    for theta in np.arange(0, np.pi, np.pi / 16):
-#        #cv2.getGaborKernel(ksize, sigma, theta, lambda, gamma, psi, ktype)
-#        kern = cv2.getGaborKernel((ksize, ksize), 4.0, theta, 10.0, 0.5, 0, ktype=cv2.CV_32F)
-#        kern /= 1.5 * kern.sum()
-#        filters.append(kern)
-#    return filters
-
-    sigma = 1.5 #spatial bandwidth:  the standard deviation of the Gaussian function used in the Gabor filter.
-    theta = np.pi/2#0 #orientation: the orientation of the normal to the parallel stripes of the Gabor function.
-    lamda = 2.0 #wavelength: the wavelength of the sinusoidal factor in the above equation.
-    gamma = 0.5 #aspect ratio:  the spatial aspect ratio.
-    psi = 0 #phase shift:  phase offset.
-    #(ksize, ksize), 4.0, theta, 10.0, 0.5, 0
-    
-    #cv2.getGaborKernel(ksize, sigma, theta, lambda, gamma, psi, ktype)
-    kern = cv2.getGaborKernel((ksize, ksize), sigma, theta, lamda, gamma, psi, ktype=cv2.CV_32F)
-    #kern /= 1.5 * kern.sum()
-    kern /= 0.5 * kern.sum()
-    filters.append(kern)
+    for lamda in lamdaList:
+        for theta in thetaList:
+            for psi in psiList:
+                # cv2.getGaborKernel(ksize, sigma, theta, lambda, gamma, psi, ktype)
+                #ksize = int(lamda)+1
+                sigma = float(lamda/np.pi*math.sqrt(math.log(2)/2)*(2**bw+1)/(2**bw-1))
+                kern = cv2.getGaborKernel((ksize, ksize), sigma, theta, lamda, gamma, psi, ktype=cv2.CV_32F)
+                #kern /= 1.5 * kern.sum()
+                #kern /= 0.2 * kern.sum()
+                filters.append(kern)
     return filters
 
 
  
 def process(img, filters):
-    accum = np.zeros_like(img)
+    filteredImages = [];
     for kern in filters:
         fimg = cv2.filter2D(img, cv2.CV_8UC3, kern)
-        np.maximum(accum, fimg, accum)
-    return accum
- 
-if __name__ == '__main__':
+        fimg = fimg/mean(fimg);
+        filteredImages.append(fimg)
+    return filteredImages
+
+
+
+def imageLoad(img_fn):
     import sys
     
-    print __doc__
-    try:
-        img_fn = sys.argv[1]
-    except:
-        img_fn = 'test.png'
+    img_fn = 'test2.png'
     
     img = cv2.imread(img_fn)
     if img is None:
@@ -52,7 +45,40 @@ if __name__ == '__main__':
     
     filters = build_filters()
     
-    res1 = process(img, filters)
-    cv2.imshow('result', res1)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    res = process(img, filters)
+    
+    
+    for r in res:
+        cv2.imshow('result', r)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        
+    return res;
+        
+
+
+# if __name__ == '__main__':
+#     import sys
+#     
+#     print __doc__
+#     try:
+#         img_fn = sys.argv[1]
+#     except:
+#         img_fn = 'test2.png'
+#     
+#     img = cv2.imread(img_fn)
+#     if img is None:
+#         print 'Failed to load image file:', img_fn
+#         sys.exit(1)
+#     
+#     filters = build_filters()
+#     
+#     res = process(img, filters)
+#     
+#     
+#     for r in res:
+#         cv2.imshow('result', r)
+#         cv2.waitKey(0)
+#         cv2.destroyAllWindows()
+#         
+    
