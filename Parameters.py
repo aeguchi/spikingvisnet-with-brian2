2f222fd1e22be869317de423f2ee5996a791b6c9
aeguchi <aki.hero.ox@gmail.com>
2015-06-30 14:11:54 +0100

    fixed: input neurons with different filters now run simultaneously. (number of the input neuron is now multiple of the number of filter)

--- Parameters.py
+++ Parameters.py
@@ -5,10 +5,10 @@ from cmath import sqrt, log
 nStim = 2
 nTrans = 8
 
-ksize = 11#31  # 31 the size of the Gabor kernel. If ksize = (a, b), we then have a Gabor kernel of size a x b pixels. As with many other convolution kernels, ksize is preferably odd and the kernel is a square (just for the sake of uniformity).
+ksize = 31#31  # 31 the size of the Gabor kernel. If ksize = (a, b), we then have a Gabor kernel of size a x b pixels. As with many other convolution kernels, ksize is preferably odd and the kernel is a square (just for the sake of uniformity).
 bw = 1.5 #spatial bandwidth:
 thetaList = [0, np.pi/4, np.pi/2, np.pi*3/4]  #0 #orientation: the orientation of the normal to the parallel stripes of the Gabor function.
-lamdaList = [5.0]; #wavelength: the wavelength of the sinusoidal factor in the above equation.
+lamdaList = [6.0]; #wavelength: the wavelength of the sinusoidal factor in the above equation.
 gamma = 0.5 #aspect ratio:  the spatial aspect ratio.
 #psiList = [0, np.pi, -np.pi/2, np.pi/2]#phase shift:  phase offset
 psiList = [0]#phase shift:  phase offset
@@ -19,7 +19,11 @@ simulationTime = 500;
 layerDim = 10;
 
 
-I_e = 180.0;
-tau_m = 20.0;
-Vth=-55.                  
-Vrest=-70.  
+I_e = 180.0;         # (nA) external current
+tau_m = 20.0;      # (ms) membrane time constant
+V_th=-55.      # (mV) firing threshold                  
+E_L=-70.       # (mV) resting potential
+
+#C_m = 250.0       # (pF) capacitance
+#V_reset = -70.0   # (mV) reset potential
+#t_ref = 2.0       # (ms) refractory period
