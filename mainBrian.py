6522c36ba51b6fcc068c294fe3873a5f01214a4c
aeguchi <aeguchi@localhost>
2015-07-30 19:01:29 +0100

    translated into Brian but still need to learn how to inject current to represent the Gabor input etc

--- mainBrian.py
+++ mainBrian.py
@@ -5,45 +5,53 @@ import numpy,pylab as plt , glob
 #import nest
 from brian2 import *
 import os
+#from nest import raster_plot
 #import nest.topology as topp
 
-### Constructing Network ###
-#nest.Models()
-#ndict = {"C_m":C_m, 'tau_m': tau_m, 't_ref': t_ref, 'E_L': E_L, 'V_th': V_th, 'V_reset': V_reset};
-#nest.SetDefaults("iaf_neuron", ndict)
-#nest.SetDefaults("stdp_synapse",stdp_dict);
-
+#http://brian2.readthedocs.org/en/latest/resources/tutorials/1-intro-to-brian-neurons.html
 
 plotGabor = 1;
 plotLayer = 0; #0:each images, 1: at end
 
+
 #Creating Gabor input layer
 layerG = [];
+rates=zeros(layerGDim*layerGDim)*Hz
 for theta in range(0,len(thetaList)):
-    layerG.append(PoissonGroup(layerGDim*layerGDim))
+    #layerG.append(NeuronGroup(layerGDim*layerGDim, eqs, threshold='v>1', reset='v = 0'))
+    layerG.append(NeuronGroup(layerGDim*layerGDim, eqs, threshold='v>-50*mV', reset='v=-60*mV'))
+    
 
 #Creating ExcitLayers
 layers = []
 for layer in range(0,nLayers):
-    layers.append(NeuronGroup(layerDim*layerDim, eqs, threshold=-50*mV, reset=-60*mV))
+    #layers.append(NeuronGroup(layerDim*layerDim, eqs, threshold='v>1', reset='v = 0'))
+    layers.append(NeuronGroup(layerDim*layerDim, eqs, threshold='v>-50*mV', reset='v=-60*mV'))
 
 #Creating InhibLayers
 inhibLayers = [] 
 for layer in range(0,nLayers):
-    inhibLayers.append(NeuronGroup(inhibLayer1Dim*inhibLayer1Dim, eqs, threshold=-50*mV, reset=-60*mV))
+    #inhibLayers.append(NeuronGroup(inhibLayerDim*inhibLayerDim, eqs, threshold='v>1', reset='v = 0'))
+    inhibLayers.append(NeuronGroup(inhibLayerDim*inhibLayerDim, eqs, threshold='v>-50*mV', reset='v=-60*mV'))
 
 #Connecting neurons in Gabor input layer and neurons in the first ExcitLayer
 connGtoInput = []
 for theta in range(0,len(thetaList)):
-    connGtoInput.append(Connection(layerG[theta],layers[0],sparseness=.5,weight=topomap)) 
-    #topp.ConnectLayers(layerG[theta], layers[0], connGDict)
+    #connGtoInput.append(Synapses(layerG[theta], layers[0], pre='v_post += 0.2'));
+    connGtoInput.append(Synapses(layerG[theta], layers[0], pre='ge+=1.62*mV'));
+    connGtoInput[theta].connect(True, p=0.02)
 
 #Connecting neurons between layers
 connFeedForward = []
 connBackProjection = []
 for layer in range(0,nLayers-1): 
-    connFeedForward.append(Connection(layers[layer],layers[layer+1],sparseness=.5,weight=topomap))
-    connBackProjection.append(Connection(layers[layer+1],layers[layer],sparseness=.5,weight=topomap))
+    #connFeedForward.append(Synapses(layers[layer],layers[layer+1], pre='v_post += 0.2'))
+    connFeedForward.append(Synapses(layers[layer],layers[layer+1], pre='ge+=1.62*mV'))
+    connFeedForward[layer].connect(True, p=0.02);
+    
+    #connBackProjection.append(Synapses(layers[layer+1],layers[layer], pre='v_post += 0.2'))
+    connBackProjection.append(Synapses(layers[layer+1],layers[layer], pre='ge+=1.62*mV'))
+    connBackProjection[layer].connect(True, p=0.02);
     
 #Connecting neurons within layers    
 connExIn = []
@@ -51,10 +59,13 @@ connInEx = []
 connRecIn = []
 connRecEx = []
 for layer in range(0,nLayers): 
-    ConnExIn.append(Connection(layers[layer],inhibLayers[layer],sparseness=.5,weight=topomap))
-    ConnInEx.append(Connection(inhibLayers[layer], layers[layer],sparseness=.5,weight=topomap))
-    #ConnRecIn.append(Connection(inhibLayers[layer],inhibLayers[layer],sparseness=.5,weight=topomap))
-    #ConnRecEx.append(Connection(layers[layer], layers[layer],sparseness=.5,weight=topomap))
+    #connExIn.append(Synapses(layers[layer],inhibLayers[layer], pre='v_post += 0.2'))
+    connExIn.append(Synapses(layers[layer],inhibLayers[layer], pre='ge+=1.62*mV'))
+    connExIn[layer].connect(True, p=0.02)
+    
+    #connInEx.append(Synapses(inhibLayers[layer], layers[layer], pre='v_post += 0.2'))
+    connInEx.append(Synapses(inhibLayers[layer], layers[layer], pre='gi-=9*mV'))
+    connInEx[layer].connect(True, p=0.02)
 
 spikesG=[]
 for theta in range(0,len(thetaList)):
@@ -92,36 +103,22 @@ for img_fn in img_fns:
     filters = build_filters()
     res = process(img, filters)
     
-#     for r in res:
-#         r= cv2.resize(r,(layerDim,layerDim),interpolation = cv2.INTER_NEAREST);
-#     
-    
     #convert from gabor filtered inputs to spikes
     #normalize
     res_norm=res/numpy.max(res);
     res_norm=1-res_norm;
-    res_lined = numpy.reshape(res_norm,(1,layerGDim*layerGDim));
-    
     
     for index_filter in range(0,len(thetaList)):
-        r = res_norm[index_filter]
-        for index_cell in range(0,layerGDim*layerGDim):
-            #y_index = int(math.floor(index_cell/layerGDim));
-            #x_index = index_cell%layerGDim;
-            layerG[index_filter][index_cell].rates = res_lined*30*Hz;
-            #nest.SetStatus([neuron], {"V_m": E_L+(V_th-E_L)*numpy.random.rand()})
-            #nest.SetStatus([neuron], {"I_e": I_e+80*mean(r[y_index][x_index])}) #TO-DO: TOBE fixed
-
-#     if(plotGabor or plotLayer==0):
-#         nest.SetStatus(spkdetG, [{"n_events": 0}]);
-#         for layer in range(0,nLayers):
-#             nest.SetStatus(spkdetLayers[layer], [{"n_events": 0}])
-#             nest.SetStatus(spkdetInhibLayers[layer], [{"n_events": 0}])
+        r = numpy.reshape(numpy.mean(res_norm[index_filter],axis=2),(layerGDim*layerGDim));
+        layerG[index_filter].v = -60*mV + r*20*mV;    #To be fixed
+        
+        
+        
+    run(simulationTime*ms)
     
     
-    #nest.Simulate(simulationTime)
     
-    run(simulationTime)
+    
     
     if(plotGabor):
         plt.figure(1)
@@ -139,32 +136,21 @@ for img_fn in img_fns:
             
             
             res_FRMap = numpy.zeros((layerGDim, layerGDim));
-            
-            dSD =nest.GetStatus(spkdetG,keys='events')[index_filter];
-            evs = dSD["senders"]
-            ts = dSD["times"]
-                
-            headNodeIndex = layerG[index_filter][0]
-            for sender in evs:
-                cell_index=sender-headNodeIndex-1
-                y_index = math.floor((cell_index)/layerGDim);
-                x_index = (cell_index)%layerGDim;
-                res_FRMap[y_index][x_index]+=1*(1000/simulationTime);
-         
+                     
             #plot spike raster
             ax=plt.subplot(5,3,(index_filter+1)*3+2);
             if(index_filter==0):
                 plt.title('Raster Plot')
-            plt.plot(ts, evs,'.')
-            ax.get_yaxis().set_visible(False)
-            ax.set_xlim([index_img*simulationTime, (index_img+1)*simulationTime])
-    
+            plot(spikesG[index_filter].t/ms, spikesG[index_filter].i, '.')
+            
             #plot FR map
             plt.subplot(5,3,(index_filter+1)*3+3);
             if(index_filter==0):
                 plt.title('Firing Rate Map')
             plt.imshow(res_FRMap,interpolation='none',vmin=0, vmax=res_FRMap.max());
             plt.colorbar();
+        plt.show();
+        
         
     if (plotLayer==0):
         plt.figure(2);    
@@ -174,24 +160,12 @@ for img_fn in img_fns:
         
         for layer in range(0,nLayers):
             
-            ex_FRMap = numpy.zeros((layer1Dim, layer1Dim));
-            #dSD =nest.GetStatus(spkdetLayers[layer],keys='events')[0];
-            #evs = dSD["senders"]
-            #ts = dSD["times"]
-                
-            #headNodeIndex = layers[layer][0]
-            #for sender in evs:
-            #    cell_index=sender-headNodeIndex-1
-            #    y_index = math.floor((cell_index)/layer1Dim);
-            #    x_index = (cell_index)%layer1Dim;
-            #    ex_FRMap[y_index][x_index]+=1*(1000/simulationTime);
-         
+            ex_FRMap = numpy.zeros((layerDim, layerDim));
             #plot spike raster
             ax=plt.subplot(nLayers+1,4,(nLayers-layer)*4+1);
             
             plt.title(layer)
-            raster_plot(spkdetLayers[layer]);
-            #plt.plot(ts, evs,'.')
+            plot(spkdetLayers[layer].t/ms, spkdetLayers[layer].i, '.')
             #ax.get_yaxis().set_visible(False)
             #ax.set_xlim([(index_img)*simulationTime, (index_img+1)*simulationTime])
             #ax.set_ylim([headNodeIndex+1, headNodeIndex+(layer1Dim*layer1Dim)]);
@@ -203,27 +177,12 @@ for img_fn in img_fns:
             plt.imshow(ex_FRMap,interpolation='none',vmin=0, vmax=ex_FRMap.max());
             plt.colorbar();
             
-            
-            
-            
-            
-#             inhib_FRMap = numpy.zeros((inhibLayer1Dim, inhibLayer1Dim));
-#             dSD =nest.GetStatus(spkdetInhibLayers[layer],keys='events')[0];
-#             evs = dSD["senders"]
-#             ts = dSD["times"]
-#                 
-#             headNodeIndex = inhibLayers[layer][0]
-#             for sender in evs:
-#                 cell_index=sender-headNodeIndex-1
-#                 y_index = math.floor((cell_index)/inhibLayer1Dim);
-#                 x_index = (cell_index)%inhibLayer1Dim;
-#                 inhib_FRMap[y_index][x_index]+=1*(1000/simulationTime);
-#          
-             #plot spike raster
+          
+            #plot spike raster
             ax=plt.subplot(nLayers+1,4,(nLayers-layer)*4+3);
              
             plt.title(layer)
-            raster_plot(spkdetInhibLayers[layer])
+            plot(spkdetInhibLayers[layer].t/ms, spkdetInhibLayers[layer].i, '.')
 #             plt.plot(ts, evs,'.')
 #             #ax.get_yaxis().set_visible(False)
 #             ax.set_xlim([(index_img)*simulationTime, (index_img+1)*simulationTime])
@@ -249,30 +208,22 @@ if (plotLayer==1):
     
     for layer in range(0,nLayers):
         #headNodeIndex = layers[layer][0]
-        #dSD =nest.GetStatus(spkdetLayers[layer],keys='events')[0];
-        #evs = dSD["senders"]
-        #ts = dSD["times"]
 
         #plot spike raster
         ax=plt.subplot(nLayers,4,(nLayers-layer-1)*4+1);
         
         plt.title(layer)
-        #plt.plot(ts, evs,'.')
         raster_plot(spkdetLayers[layer])
         #ax.get_yaxis().set_visible(False)
         #ax.set_xlim([(index_img)*simulationTime, (index_img+1)*simulationTime])
         #ax.set_ylim([headNodeIndex+1, headNodeIndex+(layer1Dim*layer1Dim)]);
         
         
-        #headNodeIndex = inhibLayers[layer][0]
         #dSD =nest.GetStatus(spkdetInhibLayers[layer],keys='events')[0];
-        #evs = dSD["senders"]
-        #ts = dSD["times"]
             
         #plot spike raster
         ax=plt.subplot(nLayers,4,(nLayers-layer-1)*4+3);
         plt.title(layer)
-        #plt.plot(ts, evs,'.')
         raster_plot(spkdetInhibLayers[layer])
         #ax.get_yaxis().set_visible(False)
         #ax.set_xlim([(index_img)*simulationTime, (index_img+1)*simulationTime])
