6c26892c73da6e5b99c2574021e56dae99698a3c
atomix01 <thomas.minot01@gmail.com>
2015-08-22 20:55:00 +0100

    decode func

--- decode.py
+++ decode.py
@@ -1,6 +1,21 @@
 def Decode():
     
+    FR_t=[nlayers][trialNb][testNb]
+    FR_b=[nlayers][trialNb]
+    
+    
     for layer in range(0,nLayers):
+        
+        for trial in range(trialNb) :
+            
+            filep= os.path.split(os.path.realpath(__file__))[0] + '/FR/' + 'FR_b_Ex'+str(layer)+'_trial'+srt(trial)
+            with open(filep,'w') as FR
+                FR_b[layer][trial] = json.load(FR)
+            
+            for test in range(testNb):
+                filep= os.path.split(os.path.realpath(__file__))[0] + '/FR/' + 'FR_b_Ex'+str(layer)+'_trial'+srt(trial)+'_test'+str(test)
+                with open(filep,'w') as FR
+                    FR_t[layer][trial][test] = json.load(FR)
         
         spikes = spkdetLayers[layer].spike_trains();
         
