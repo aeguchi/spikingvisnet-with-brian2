7946716286ad7b7b66397dcbed5579b371d4b2d3
aeguchi <aeguchi@localhost>
2015-11-09 19:33:38 +0000

    added a function to output the FR using pickle

--- main.py
+++ main.py
@@ -13,11 +13,12 @@ import pylab as plt
 import glob
 import os
 import sys
+import pickle
 
 plotGabor = 0;
 plotLayer = 0;  # 0:each images, 1: at end
-trainOnly = 1;
-
+trainOn = 1;
+testOn = 1;
 vnet = netarch.visnet()
 vplotter = netplot.plotter(vnet)
 
@@ -33,62 +34,21 @@ fileList_train = np.genfromtxt(os.path.split(os.path.realpath(__file__))[0] + "/
 numObj_train = int(fileList_train[0]);
 numTrans_train = int(fileList_train[1]);
 fileList_test = np.genfromtxt(os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/fileList_test.txt", dtype='str');numObj_train = int(fileList_train[0]);
-numTrans_test = int(fileList_test[0]);
+numObj_test = int(fileList_test[0]);
 numTrans_test = int(fileList_test[1]);
 
 
 # index_img=0
 
 vnet.setSynapticPlasticity(True)
-
-# for img_fn in img_fns:
-for index_obj in range(numObj_train):
-    for index_trans in range(numTrans_train):
-        print "obj: " + str(index_obj) + ", trans: " + str(index_trans);
-        index_img = index_obj * numTrans_train + index_trans;
-        img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/train/" + fileList_train[index_obj * numTrans_train + index_trans + 2];
-        # print img_fn
-    
-        # load gabor filtered ImageSurface
-    
-        img = cv2.imread(img_fn)
-    
-        if img is None:
-            print 'Failed to load image file:', img_fn
-            sys.exit(1)
-    
-        # filter the images and convert to normalised spikes
-        
-        filters = imimp.build_filters()
-        res = imimp.process(img, filters)
-        res_norm = imimp.to_spikes(res)
-    
-        vnet.setGaborFiringRates(res_norm)
-    
-        # run visnet simulation!
-    
-        vnet.net.run(simulationTime * ms)
-    
-        # plot each image set
-        if plotGabor:
-            vplotter.plotGaborInput(img, index_img, res, res_norm)
-        if plotLayer == 0:
-            vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
-    
-
-        # index_img += 1
-    vnet.traceReset();
-if plotLayer == 1:
-    vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
-    
-vnet.setSynapticPlasticity(False)
-
-if trainOnly == 0:
+if trainOn == 1:
     # for img_fn in img_fns:
-    for index_obj in range(numObj_test):
-        for index_trans in range(numTrans_test):
-            index_img = index_obj * numTrans_test + index_trans;
-            img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/test/" + fileList_test[index_obj * numTrans_train + index_trans + 2];
+    FRrec = np.zeros((numObj_test,numTrans_test,nLayers+1, layerDim, layerDim));#1st layer is binding layer
+    for index_obj in range(numObj_train):
+        for index_trans in range(numTrans_train):
+            print "obj: " + str(index_obj) + ", trans: " + str(index_trans);
+            index_img = index_obj * numTrans_train + index_trans;
+            img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/train/" + fileList_train[index_obj * numTrans_train + index_trans + 2];
             # print img_fn
         
             # load gabor filtered ImageSurface
@@ -112,9 +72,61 @@ if trainOnly == 0:
             vnet.net.run(simulationTime * ms)
         
             # plot each image set
-        
             if plotGabor:
                 vplotter.plotGaborInput(img, index_img, res, res_norm)
-            vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
+            if plotLayer == 0:
+                FRrecTmp = np.zeros((nLayers+1, layerDim, layerDim));
+                vplotter.plotLayers(img, index_img, FRrecTmp, plotLayer=plotLayer)
+                FRrec[index_obj][index_trans]=FRrecTmp;
+        
+    
+            # index_img += 1
+        vnet.traceReset();
+    if plotLayer == 1:
+        vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
+    #pickle.dump(FRrec, open("out1.pkl", "wb"))
+
+
+if testOn == 1:
+    vnet.setSynapticPlasticity(False)
+    FRrec = np.zeros((numObj_test,numTrans_test,nLayers+1, layerDim, layerDim));#1st layer is binding layer
+    # for img_fn in img_fns:
+    for index_obj in range(numObj_test):
+        for index_trans in range(numTrans_test):
+            print "obj: " + str(index_obj) + ", trans: " + str(index_trans);
+            index_img = index_obj * numTrans_test + index_trans;
+            img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/train/" + fileList_train[index_obj * numTrans_train + index_trans + 2];
+            # print img_fn
+        
+            # load gabor filtered ImageSurface
+        
+            img = cv2.imread(img_fn)
+        
+            if img is None:
+                print 'Failed to load image file:', img_fn
+                sys.exit(1)
+        
+            # filter the images and convert to normalised spikes
+            
+            filters = imimp.build_filters()
+            res = imimp.process(img, filters)
+            res_norm = imimp.to_spikes(res)
+        
+            vnet.setGaborFiringRates(res_norm)
+        
+            # run visnet simulation!
+        
+            vnet.net.run(simulationTime * ms)
+        
+            # plot each image set
+            if plotGabor:
+                vplotter.plotGaborInput(img, index_img, res, res_norm)
+            if plotLayer == 0:
+                FRrecTmp = np.zeros((nLayers+1, layerDim, layerDim));
+                vplotter.plotLayers(img, index_img,FRrecTmp, plotLayer=plotLayer)
+                FRrec[index_obj][index_trans]=FRrecTmp;
         
             # index_img += 1
+        vnet.traceReset();
+    pickle.dump(FRrec, open("out2.pkl", "wb"))
+    #FRrec=pickle.load(open("out2.pkl", "rb"))
