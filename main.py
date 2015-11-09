3798905fa5233a06146984c10c3b69de0d07221a
aeguchi <aeguchi@localhost>
2015-11-09 16:50:01 +0000

    synaptic connections fixed and trace reset implemented

--- main.py
+++ main.py
@@ -14,8 +14,9 @@ import glob
 import os
 import sys
 
-plotGabor = 1;
-plotLayer = 0; # 0:each images, 1: at end
+plotGabor = 0;
+plotLayer = 0;  # 0:each images, 1: at end
+trainOnly = 1;
 
 vnet = netarch.visnet()
 vplotter = netplot.plotter(vnet)
@@ -28,24 +29,25 @@ vplotter = netplot.plotter(vnet)
 #     print 'Error: the number of images files does not match',len(img_fns);
 #     sys.exit(1)
 
-fileList_train = np.genfromtxt(os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/fileList_train.txt",dtype='str');
+fileList_train = np.genfromtxt(os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/fileList_train.txt", dtype='str');
 numObj_train = int(fileList_train[0]);
 numTrans_train = int(fileList_train[1]);
-fileList_test = np.genfromtxt(os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/fileList_test.txt",dtype='str');numObj_train = int(fileList_train[0]);
+fileList_test = np.genfromtxt(os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/fileList_test.txt", dtype='str');numObj_train = int(fileList_train[0]);
 numTrans_test = int(fileList_test[0]);
 numTrans_test = int(fileList_test[1]);
 
 
-#index_img=0
+# index_img=0
 
 vnet.setSynapticPlasticity(True)
 
-#for img_fn in img_fns:
+# for img_fn in img_fns:
 for index_obj in range(numObj_train):
     for index_trans in range(numTrans_train):
-        index_img = index_obj*numTrans_train + index_trans;
-        img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/train/" + fileList_train[index_obj*numTrans_train + index_trans + 2];
-        #print img_fn
+        print "obj: " + str(index_obj) + ", trans: " + str(index_trans);
+        index_img = index_obj * numTrans_train + index_trans;
+        img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/train/" + fileList_train[index_obj * numTrans_train + index_trans + 2];
+        # print img_fn
     
         # load gabor filtered ImageSurface
     
@@ -65,48 +67,54 @@ for index_obj in range(numObj_train):
     
         # run visnet simulation!
     
-        vnet.net.run(simulationTime*ms)
+        vnet.net.run(simulationTime * ms)
     
         # plot each image set
+        if plotGabor:
+            vplotter.plotGaborInput(img, index_img, res, res_norm)
+        if plotLayer == 0:
+            vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
     
-        vplotter.plotGaborInput(img, index_img, res, res_norm)
-        vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
-    
-        #index_img += 1
+
+        # index_img += 1
+    vnet.traceReset();
+if plotLayer == 1:
+    vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
     
 vnet.setSynapticPlasticity(False)
 
-#for img_fn in img_fns:
-for index_obj in range(numObj_test):
-    for index_trans in range(numTrans_test):
-        index_img = index_obj*numTrans_test + index_trans;
-        img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/test/" + fileList_test[index_obj*numTrans_train + index_trans + 2];
-        #print img_fn
-    
-        # load gabor filtered ImageSurface
-    
-        img = cv2.imread(img_fn)
-    
-        if img is None:
-            print 'Failed to load image file:', img_fn
-            sys.exit(1)
-    
-        # filter the images and convert to normalised spikes
+if trainOnly == 0:
+    # for img_fn in img_fns:
+    for index_obj in range(numObj_test):
+        for index_trans in range(numTrans_test):
+            index_img = index_obj * numTrans_test + index_trans;
+            img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/test/" + fileList_test[index_obj * numTrans_train + index_trans + 2];
+            # print img_fn
         
-        filters = imimp.build_filters()
-        res = imimp.process(img, filters)
-        res_norm = imimp.to_spikes(res)
-    
-        vnet.setGaborFiringRates(res_norm)
-    
-        # run visnet simulation!
-    
-        vnet.net.run(simulationTime*ms)
-    
-        # plot each image set
-    
-        if plotGabor:
-            vplotter.plotGaborInput(img, index_img, res, res_norm)
-        vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
-    
-        #index_img += 1
+            # load gabor filtered ImageSurface
+        
+            img = cv2.imread(img_fn)
+        
+            if img is None:
+                print 'Failed to load image file:', img_fn
+                sys.exit(1)
+        
+            # filter the images and convert to normalised spikes
+            
+            filters = imimp.build_filters()
+            res = imimp.process(img, filters)
+            res_norm = imimp.to_spikes(res)
+        
+            vnet.setGaborFiringRates(res_norm)
+        
+            # run visnet simulation!
+        
+            vnet.net.run(simulationTime * ms)
+        
+            # plot each image set
+        
+            if plotGabor:
+                vplotter.plotGaborInput(img, index_img, res, res_norm)
+            vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
+        
+            # index_img += 1
