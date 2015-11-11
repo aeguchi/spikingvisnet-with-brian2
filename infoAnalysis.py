6fbc211cd18e5d35cbc5f242f70290f666db919d
aeguchi <aki.hero.ox@gmail.com>
2015-11-11 10:01:53 +0000

    Info analysis in python added

---
+++ infoAnalysis.py
@@ -0,0 +1,87 @@
+import numpy as np
+import pylab as plt
+import pickle
+import os;
+
+
+plotAllSingleCellInfo = 0;
+
+ExperimentName = "BOtest";
+layer = 2; #0: gabor, 1: 1st layer ..
+
+FR=pickle.load(open(os.path.split(os.path.realpath(__file__))[0] +"/Results/"+ExperimentName+"/FR_0_blank.pkl", "rb"));
+#FR=pickle.load(open(os.path.split(os.path.realpath(__file__))[0] +"/Results/"+ExperimentName+"/FR_1_trained.pkl", "rb"));
+numObj = FR.shape[0];
+numTrans = FR.shape[1];
+numLayers = FR.shape[2]-1;
+numCells = FR.shape[3];
+
+FR_layer = FR[:,:,layer,:,:];
+
+FR_norm = (FR-FR[:,:,layer,:,:].min())/(FR[:,:,layer,:,:].max()-FR[:,:,layer,:,:].min());
+FR = FR_norm;
+
+#settings
+numBins =  3;##numTrans;   #can be adjusted
+weightedAnalysis = 1;#exclude the selectivity by not responding to a particular stimulus
+#print np.around(FR[:,:,layer,:,:]*(numBins-1))
+
+multi_cell_analysis = 0; #1 to run multi-cell info analysis
+num_samples = 10;   #num samples from Max-Cells of each stimulus for multi-cell info. analysis
+sampleMulti =1 #4*4;    #default is 1; this is to increase the sampling size to deal with larger layers.
+nc_max = 15;#num_samples*numObj;   # max ensemble size for multi-cell info. analysis
+
+sumPerBin = np.zeros((numCells,numCells,numBins));
+sumPerObj = numTrans;
+sumPerCell = numTrans*numObj;
+IRs = np.zeros((numObj,numCells,numCells));#I(R,s) single cell information
+IRs_weighted = np.zeros((numObj,numCells,numCells));#I(R,s) single cell information
+pq_r = np.zeros(numObj)#prob(s') temporally used in the decoing process
+Ps = 1/numObj   #Prob(s) 
+
+Iss2_Q_avg = np.zeros(nc_max);#average quantized info for n cells; I(s,s')
+Iss2_P_avg = np.zeros(nc_max);## average smoothed info for n cells; I(s,s')
+
+
+print("**Loading data**")
+binMatrix = np.zeros((numCells, numCells, numObj, numBins));# #number of times when fr is classified into a specific bin within a specific objs's transformations
+binMatrixTrans = np.zeros((numCells, numCells, numObj, numBins, numTrans));  #TF table to show if a certain cell is classified into a certain bin at a certain transformation
+for obj in range(numObj):
+    print str(obj) + '/' + str(numObj);
+    for trans in range(numTrans):
+        for row in range(numCells):
+            for col in range(numCells):
+                bin = np.around(FR[obj,trans,layer,col,row]*(numBins-1));
+                binMatrix[row,col,obj,bin]=binMatrix[row,col,obj,bin]+1;
+                binMatrixTrans[row,col,obj,bin,trans]=1;
+
+#print binMatrix;
+
+print "** single-cell information analysis **";
+# Loop through all cells to calculate single cell information
+for row in range(numCells):
+    for col in range(numCells):
+        # For each cell, count the number of transforms per bin
+        for bin in range(numBins):
+            for obj in range(numObj):
+                sumPerBin[row,col,bin]+=binMatrix[row,col,obj,bin];
+
+        # Calculate the information for cell_x cell_y per stimulus
+        for obj in range(numObj):
+            for bin in range(numBins):
+                Pr = sumPerBin[row,col,bin]/sumPerCell;
+                Prs = binMatrix[row,col,obj,bin]/sumPerObj;
+                if(Pr!=0 and Prs!=0 and Pr<Prs):
+                    IRs[obj,row,col]+=(Prs*(np.log2(Prs/Pr)));#*((bin-1)/(numBins-1)); #could be added to weight the degree of firing rates.
+                    #IRs(row,col,obj)=IRs(row,col,obj)+(Prs*(log2(Prs/Pr)))*((bin-1)/(numBins-1)); #could be added to weight the degree of firing rates.
+                    IRs_weighted[obj,row,col]+=(Prs*(np.log2(Prs/Pr)))*((bin)/(numBins-1)); #could be added to weight the degree of firing rates.
+ 
+if (weightedAnalysis==1):
+    IRs = IRs_weighted;
+
+IRs_reshaped = np.reshape(IRs,(numObj,numCells*numCells));
+IRs_sorted = np.transpose(np.sort(IRs_reshaped));
+
+plt.plot(IRs_sorted);
+plt.show();
+    
--- main.py
+++ main.py
@@ -14,11 +14,25 @@ import glob
 import os
 import sys
 import pickle
+import errno
 
 plotGabor = 0;
 plotLayer = 0;  # 0:each images, 1: at end
-trainOn = 1;
-testOn = 1;
+phases = [0,1,2] #testing only [0], testing and training [0,2]
+
+
+try:
+    os.makedirs("Results");
+except OSError as exception:
+    if exception.errno != errno.EEXIST:
+        raise
+
+try:
+    os.makedirs("Results/"+experimentName);
+except OSError as exception:
+    if exception.errno != errno.EEXIST:
+        raise
+
 vnet = netarch.visnet()
 vplotter = netplot.plotter(vnet)
 
@@ -30,6 +44,9 @@ vplotter = netplot.plotter(vnet)
 #     print 'Error: the number of images files does not match',len(img_fns);
 #     sys.exit(1)
 
+
+
+
 fileList_train = np.genfromtxt(os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/fileList_train.txt", dtype='str');
 numObj_train = int(fileList_train[0]);
 numTrans_train = int(fileList_train[1]);
@@ -37,11 +54,12 @@ fileList_test = np.genfromtxt(os.path.split(os.path.realpath(__file__))[0] + "/i
 numObj_test = int(fileList_test[0]);
 numTrans_test = int(fileList_test[1]);
 
-
-# index_img=0
-
-vnet.setSynapticPlasticity(True)
-if trainOn == 1:
+for phase in [0,1,2]:
+    if phase==1:
+        vnet.setSynapticPlasticity(True)
+    else:
+        vnet.setSynapticPlasticity(False)
+        
     # for img_fn in img_fns:
     FRrec = np.zeros((numObj_test,numTrans_test,nLayers+1, layerDim, layerDim));#1st layer is binding layer
     for index_obj in range(numObj_train):
@@ -77,56 +95,59 @@ if trainOn == 1:
             if plotLayer == 0:
                 FRrecTmp = np.zeros((nLayers+1, layerDim, layerDim));
                 vplotter.plotLayers(img, index_img, FRrecTmp, plotLayer=plotLayer)
-                FRrec[index_obj][index_trans]=FRrecTmp;
+                FRrec[index_obj,index_trans]=FRrecTmp;
         
     
             # index_img += 1
         vnet.traceReset();
     if plotLayer == 1:
-        vplotter.plotLayers(img, index_img, plotLayer=plotLayer)
-    #pickle.dump(FRrec, open("out1.pkl", "wb"))
+        vplotter.plotLayers(img, index_img, FRrecTmp, plotLayer=plotLayer)
+    if phase==0:
+        pickle.dump(FRrec, open("Results/"+experimentName+"/FR_0_blank.pkl", "wb"))
+    elif phase==2:
+        pickle.dump(FRrec, open("Results/"+experimentName+"/FR_1_trained.pkl", "wb"))
 
 
-if testOn == 1:
-    vnet.setSynapticPlasticity(False)
-    FRrec = np.zeros((numObj_test,numTrans_test,nLayers+1, layerDim, layerDim));#1st layer is binding layer
-    # for img_fn in img_fns:
-    for index_obj in range(numObj_test):
-        for index_trans in range(numTrans_test):
-            print "obj: " + str(index_obj) + ", trans: " + str(index_trans);
-            index_img = index_obj * numTrans_test + index_trans;
-            img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/train/" + fileList_train[index_obj * numTrans_train + index_trans + 2];
-            # print img_fn
-        
-            # load gabor filtered ImageSurface
-        
-            img = cv2.imread(img_fn)
-        
-            if img is None:
-                print 'Failed to load image file:', img_fn
-                sys.exit(1)
-        
-            # filter the images and convert to normalised spikes
-            
-            filters = imimp.build_filters()
-            res = imimp.process(img, filters)
-            res_norm = imimp.to_spikes(res)
-        
-            vnet.setGaborFiringRates(res_norm)
-        
-            # run visnet simulation!
-        
-            vnet.net.run(simulationTime * ms)
-        
-            # plot each image set
-            if plotGabor:
-                vplotter.plotGaborInput(img, index_img, res, res_norm)
-            if plotLayer == 0:
-                FRrecTmp = np.zeros((nLayers+1, layerDim, layerDim));
-                vplotter.plotLayers(img, index_img,FRrecTmp, plotLayer=plotLayer)
-                FRrec[index_obj][index_trans]=FRrecTmp;
-        
-            # index_img += 1
-        vnet.traceReset();
-    pickle.dump(FRrec, open("out2.pkl", "wb"))
+# if testOn == 1:
+#     vnet.setSynapticPlasticity(False)
+#     FRrec = np.zeros((numObj_test,numTrans_test,nLayers+1, layerDim, layerDim));#1st layer is binding layer
+#     # for img_fn in img_fns:
+#     for index_obj in range(numObj_test):
+#         for index_trans in range(numTrans_test):
+#             print "obj: " + str(index_obj) + ", trans: " + str(index_trans);
+#             index_img = index_obj * numTrans_test + index_trans;
+#             img_fn = os.path.split(os.path.realpath(__file__))[0] + "/images/" + imageFolder + "/train/" + fileList_train[index_obj * numTrans_train + index_trans + 2];
+#             # print img_fn
+#         
+#             # load gabor filtered ImageSurface
+#         
+#             img = cv2.imread(img_fn)
+#         
+#             if img is None:
+#                 print 'Failed to load image file:', img_fn
+#                 sys.exit(1)
+#         
+#             # filter the images and convert to normalised spikes
+#             
+#             filters = imimp.build_filters()
+#             res = imimp.process(img, filters)
+#             res_norm = imimp.to_spikes(res)
+#         
+#             vnet.setGaborFiringRates(res_norm)
+#         
+#             # run visnet simulation!
+#         
+#             vnet.net.run(simulationTime * ms)
+#         
+#             # plot each image set
+#             if plotGabor:
+#                 vplotter.plotGaborInput(img, index_img, res, res_norm)
+#             if plotLayer == 0:
+#                 FRrecTmp = np.zeros((nLayers+1, layerDim, layerDim));
+#                 vplotter.plotLayers(img, index_img,FRrecTmp, plotLayer=plotLayer)
+#                 FRrec[index_obj][index_trans]=FRrecTmp;
+#         
+#             # index_img += 1
+#         vnet.traceReset();
+#     pickle.dump(FRrec, open("out2.pkl", "wb"))
     #FRrec=pickle.load(open("out2.pkl", "rb"))
